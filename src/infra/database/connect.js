78459e5fb5a1324f78e7834d9de87c45b94e21dd
Gabriel Vilela <gabrielvilela@MacBook-Air-de-Gabriel.local>
2023-02-06 21:22:37 -0300

    troca de variavel

--- src/infra/database/connect.js
+++ src/infra/database/connect.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host: process.env.HOST,
+  host: process.env.DB_HOST,
   port: process.env.PORTSQL,
   user: process.env.DB_USER,
-  password: process.env.PASSWORD,
+  password: process.env.DB_PASSWORD,
   database: process.env.DATABASE,
 });
 
