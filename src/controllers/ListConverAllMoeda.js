80fa6080b80728866a4bd35da11ba3686d99f3f4
Gabriel Vilela <gabrielvilela@Air-de-Gabriel.local>
2023-02-08 01:28:17 -0300

    criação do endpoit de conversão de moeda

--- src/controllers/ListConverAllMoeda.js
+++ src/controllers/ListConverAllMoeda.js
@@ -1,13 +1,13 @@
 const db = require("../infra/database/connect");
 const axios = require("axios");
 
-class ListConverMoeda {
-  static async TrazCoverMoeda(req, res) {
+class ListConverAllMoeda {
+  static async TrazCoverAllMoedas(req, res) {
     const api = axios.create({
       baseURL: `https://economia.awesomeapi.com.br/`,
     });
 
-    const { token, conversao } = req.params;
+    const { conversao } = req.params;
 
     if (conversao != "EUR" && conversao != "USD") {
       return res.status(400).json({ msg: `Metodo ${conversao} e invalido` });
@@ -19,10 +19,8 @@ class ListConverMoeda {
 
         var { data } = await api.get(`${conversao}`);
 
-        db.query(
-          "SELECT * FROM api WHERE token = ?",
-          [token],
-          (err, result) => {
+        db.query("SELECT * FROM api", (err, result) => {
+          for (var i = 0; i < 10; i++) {
             var valor = result[0].valor / data[0].bid;
 
             return res.json({
@@ -33,17 +31,15 @@ class ListConverMoeda {
               img: result[0].img,
             });
           }
-        );
+        });
         break;
       case "USD":
         console.log(`Conversão em DOLAR`);
 
         var { data } = await api.get(`${conversao}`);
 
-        db.query(
-          "SELECT * FROM api WHERE token = ?",
-          [token],
-          (err, result) => {
+        db.query("SELECT * FROM api", (err, result) => {
+          for (var i = 0; i < 10; i++) {
             var valor = result[0].valor / data[0].bid;
 
             return res.json({
@@ -54,11 +50,11 @@ class ListConverMoeda {
               img: result[0].img,
             });
           }
-        );
+        });
 
         break;
     }
   }
 }
 
-module.exports = { ListConverMoeda };
+module.exports = { ListConverAllMoeda };
